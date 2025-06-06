01cdaa6f8b7b769c85cca76d1c116a7a2db3d143
Ghilaine17 <cardinalealessia3@gmail.com>
2025-06-06 19:40:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let wrapper = document.querySelector(".wrapper")
 
-for (i = 1; i <= 100; i = i + 1) {
+for (let i = 1; i <= 100; i = i + 1) {
     if(i % 3 === 0 && i % 5 === 0){
         wrapper.innerHTML += 
         `<div class="my-square d-flex bg-success bg-opacity-75  justify-content-center align-items-center">
