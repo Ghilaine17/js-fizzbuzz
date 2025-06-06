c231799839e9172a253e1814534f17d8b014d9e5
Ghilaine17 <cardinalealessia3@gmail.com>
2025-06-06 19:00:11 +0800

    added a list of number with js

---
+++ script.js
@@ -0,0 +1,25 @@
+let wrapper = document.querySelector(".wrapper")
+
+for (i = 1; i <= 100; i = i + 1) {
+    if(i % 3 === 0 && i % 5 === 0){
+        wrapper.innerHTML += 
+        `<div class="my-square d-flex bg-success bg-opacity-75  justify-content-center align-items-center">
+            Fizz-Buzz
+        </div>`
+    }else if(i % 5 === 0){
+          wrapper.innerHTML += 
+        `<div class="my-square bg-primary d-flex bg-opacity-75  justify-content-center align-items-center">
+            Buzz
+        </div>`
+    }else if( i % 3 === 0){
+         wrapper.innerHTML += 
+        `<div class="my-square bg-warning bg-opacity-75  d-flex justify-content-center align-items-center">
+            Fizz
+        </div>`
+    } else{
+          wrapper.innerHTML += 
+        `<div class="my-square d-flex bg-danger-subtle bg-opacity-75 justify-content-center align-items-center">
+            ${i}
+        </div>`
+    }
+}
